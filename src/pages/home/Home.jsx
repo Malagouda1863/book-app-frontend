00abaa6095affda9d3ec7fb41f5b1affe96ef5ee
Malagouda1863 <malangoudapatil7788@gmail.com>
2025-03-23 11:28:56 +0530

    first commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSellers from './TopSellers'
-import Recommened from './recommened'
+import Recommened from './Recommened'
 import News from './News'
 
 const Home = () => {
